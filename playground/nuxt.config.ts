baec6b78f0b392e23c0a507c76db9923958ba055
wenqing <wenqing@kerrylan.com>
2023-12-25 10:01:36 +0800

    feat: upgrade h3-proxy to v1.11

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -2,36 +2,36 @@ export default defineNuxtConfig({
   modules: ['../src/module'],
 
   // test function value
-  proxy: {
-    options: [
-      {
-        target: 'http://16.163.143.182',
-        pathRewrite: { '^/api2': '' },
-        pathFilter: function (path) {
-          const re = new RegExp(`^/api2.*`)
-          return re.test(path)
-        },
-        configureProxyRequest: function (event) {
-          console.log(event.node.req.headers)
-          return {
-            headers: {
-              token: 'set some token value',
-            },
-          }
-        },
-      },
-    ],
-  },
-
   // proxy: {
   //   options: [
   //     {
   //       target: 'http://16.163.143.182',
   //       pathRewrite: { '^/api2': '' },
-  //       pathFilter: ['/api2/**'],
+  //       pathFilter: function (path) {
+  //         const re = new RegExp(`^/api2.*`)
+  //         return re.test(path)
+  //       },
+  //       configureProxyRequest: function (event) {
+  //         console.log(event.node.req.headers)
+  //         return {
+  //           headers: {
+  //             token: 'set some token value',
+  //           },
+  //         }
+  //       },
   //     },
   //   ],
   // },
+
+  proxy: {
+    options: [
+      {
+        target: 'http://16.163.143.182',
+        pathRewrite: { '^/api2': '' },
+        pathFilter: ['/api2/**'],
+      },
+    ],
+  },
 
   // or use runtimeConfig
   // runtimeConfig: {
